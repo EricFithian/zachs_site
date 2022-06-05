7b5cf149d9a4445955eac90ff43d7d40f80e2e2e
Eric Fithian <ef2413@columbia.edu>
2022-06-05 17:35:22 -0400

    Started zach's site

---
+++ controllers/tutorial.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const router = express.Router();
+
+const db = require('../models/index')
+
+router.get('/', async (req, res, next) => {
+    try {
+        // if(!req.session) res.redirect('/login')
+        const tutorials = await db.Tutorial.find({});
+        const context = { tutorials }
+        console.log(tutorials);
+        return res.render('tutorial_index.ejs', context);
+    } catch (error) {
+        console.log(error);
+        req.error = error;
+        return next();
+    }
+});
+
+router.get('/new', (req, res) => {
+    res.render('new.ejs')
+})
+
+router.post('/', async (req, res, next) => {
+    try {
+        // console.log(`The req.body is ${req.body}`)
+        const createdTutorial = await db.Tutorial.create(req.body);
+        console.log(`The created product is ${createdTutorial}`)
+        res.redirect('/blogs');
+    } catch (error) {
+        console.log(error);
+        req.error = error;
+        return next();
+    }
+})
+
+module.exports = router
---
+++ models/Tutorial.js
@@ -0,0 +1,33 @@
+// models/Product.js
+
+const mongoose = require('mongoose');
+
+const tutorialSchema = new mongoose.Schema({
+    author: {
+        type: String,
+        required: [true, 'author cannot be empty :(']
+    },
+    thumbnail: {
+        type: String,
+    },
+    title: {
+        type: String,
+        required: [true, 'title cannot be empty!']
+    },
+    header: {
+        type: String,
+    },
+    body: {
+        type: String,
+        required: [true, 'body cannot be empty!']
+    },
+},
+    {
+        timestamps: true
+    }
+);
+
+// mongoose.model(<mongodb collection name>, our schema)
+const Tutorial = mongoose.model('Tutorial', tutorialSchema);
+
+module.exports = Tutorial;
